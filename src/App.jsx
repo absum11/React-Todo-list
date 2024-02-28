3d829854f780162c34f0e00ee0504993d8a22d47
Abhinav Pathak <abhinavpathak398@gmail.com>
2024-02-29 03:49:15 +0530

    feat: added task list state and submithandler

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,54 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const[newTask,setNewTask]=useState("")
+  const [newTask, setNewTask] = useState("");
+  const [todos, setTodos] = useState([]);
+  
+  // handle updating task list state
+  function handleSubmit(e) {
+    e.preventDefault(); //prevents page from refreshing
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newTask, completed: false },
+      ];
+    });
+    setNewTask("")
+  }
+  
+
+
   return (
     <>
-      <form className="new-task-form">
+      <form onSubmit={handleSubmit} className="new-task-form">
         <div className="form-row">
           <label htmlFor="task">New Task</label>
-          <input value={newTask}
-           onChange={e => setNewTask(e.target.value)}
-           type="text"
-            id="task" />
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            type="text"
+            id="task"
+          />
         </div>
         <button className="btn">Add</button>
       </form>
       <h1 className="header">Todo list</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.length === 0 && "No task added"}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" 
+                checked={todo.completed}
+                 />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
